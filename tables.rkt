2ee3479d57e05bde6b76d7c24e5660ece0b219de
Oussam <larkem.m.oussam@gmail.com>
2023-01-11 20:15:38 +0100

    fix a bug in the 1d-tables

--- tables.rkt
+++ tables.rkt
@@ -17,16 +17,21 @@
     (define (get-value key)
       (define (lookup key tail)
         (cond ((null? tail) #f)
-              ((same-key? (caar tail)) (cdar tail))
+              ((same-key? key (car (car tail))) (cdr (car tail)))
               (else (lookup key (cdr tail)))))
       (lookup key (cdr head)))
-            
+
+    (define (get-head) head)
 
     (define (dispatch m)
       (cond ((eq? m 'insert-value!) insert-value!)
             ((eq? m 'get-value) get-value)
+            ((eq? m 'head) (get-head))
             (else (error "unknown function"))))
     dispatch))
 
 (define t1 (named-1-table 'my-table eq?))
 ((t1 'insert-value!) 'name 'oussam)
+((t1 'insert-value!) 'age 34)
+(t1 'head)
+((t1 'get-value) 'address)
