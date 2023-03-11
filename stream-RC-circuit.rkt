36d40cb5029643ff777f3f9e7d8ae27dcaf6c97e
Oussam <larkem.m.oussam@gmail.com>
2023-03-11 20:35:22 +0100

    Solving 3.78

--- stream-RC-circuit.rkt
+++ stream-RC-circuit.rkt
@@ -57,27 +57,57 @@
 
 
 ;; exercise 3.74
-(define (stream-map f . streams)
+(define (stream-map_ f . streams)
   (stream-cons (apply f (map stream-first streams))
-               (apply stream-map (cons f (map stream-rest streams)))))
+               (apply stream-map_ (cons f (map stream-rest streams)))))
 
 (define (crossing input-stream)
   (define (crossing-function x0 x1)
     (let ((prod (* x0 x1)))
     (if (>= prod 0) 0
                    (if (< x0 0) 1 -1))))
-  (stream-map crossing-function input-stream (stream-rest input-stream)))
+  (stream-map_ crossing-function input-stream (stream-rest input-stream)))
 
 ;; ex 3.75 done on paper
 ;; ex 3.76
 
 (define (smooth s)
   (define (avg a b) (/ (+ a b) 2))
-  (stream-map avg s (stream-rest s)))
+  (stream-map_ avg s (stream-rest s)))
 
 (define (smoothed-zero-crossing smoothing-function s)
   (crossing (smoothing-function s)))
 
+;; streams and delayed evaluation: 3.5.4
 
+(define (integrate-delayed integrand v0 dt)
+  (define int (stream-cons v0 (stream-add int (stream-scale (integrand) dt))))
+  int)
 
+;;solving equation dy/dt = f(y), and y(0) = y0
 
+(define (solve-eq1 f y0 dt)
+  (define (fy) (stream-map f y))
+  (define y (integrate-delayed fy y0 dt))
+  y)
+
+;; ex 3.77
+
+(define (integrate-delayed2 integrand v0 dt)
+  (stream-cons v0
+               (integrate-delayed2 (lambda () (stream-rest (integrand)))
+                                   (+ v0 (* dt (stream-first (integrand))))
+                                   dt)))
+
+(define (solve-eq1-v2 f y0 dt)
+  (define (fy) (stream-map f y))
+  (define y (integrate-delayed2 fy y0 dt))
+  y)
+
+;;tests
+;(define e (solve-eq1-v2 (lambda (x) x) 1 0.001))
+;(stream-ref e 1000)
+
+;; ex 3.78
+(define (solve-2nd a b y0 dy0)
+  0)
