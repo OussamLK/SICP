f5d0bdba3895bef7e9b3c19d303ad69ae2ebe96d
Oussam Larkem <larkem.m.oussam@gmail.com>
2023-03-28 21:04:16 +0100

    add tests to metacircular evaluator

--- metacircular-evaluator.rkt
+++ metacircular-evaluator.rkt
@@ -1,5 +1,5 @@
-#lang racket
-
+#lang racket/base
+(require rackunit)
 
 (define ns (make-base-namespace))
 
@@ -34,23 +34,22 @@
             (else (error "unknow command" m))))
     dispatch))
 
-(define [evaluate-operands exp env]
-  (define (eval-ops operands env)
-      ;(map display (list "looping for operands" operands)) 
-      (if (null? operands) '() (cons [ev (car operands) env] [eval-ops (cdr operands) env])))
-  (cons (car exp) (eval-ops (cdr exp) env)))
-
 
 (define (ev exp env)
-  (define (atomic-procedure? exp) (member (car exp) '(+ - * /)))
+  (define (primitive-procedure? exp) (member (car exp) '(+ - * /)))
+  (define (get-value sym) (ev sym env))
   (cond ((number? exp) exp)
         ((symbol? exp) ([env 'get-binding] exp))
         ((pair? exp)
-         (cond ((atomic-procedure? exp) (eval [evaluate-operands exp env] ns))
+         (cond ((primitive-procedure? exp) (eval (cons (car exp)
+                                                    [map get-value (cdr exp)])
+                                              ns))
                ((eq? 'define (car exp)) ((env 'set-binding!) (cadr exp) (ev (caddr exp) env) ))
                ((eq? 'quote (car exp)) exp)
                ((eq? 'lambda (car exp)) (create-function (cadr exp) (caddr exp) env))
-               (else (error "I do not know how to evaluate " (car exp)))
+               (else (let ([function (ev (car exp) env)])
+                       (if function (apply function [map get-value (cdr exp)] )
+                           (error "I can not find function: " (car exp)))))
            ))
         (else (error "I dont know how to evaluate " exp))))
 
@@ -63,28 +62,62 @@
           (else (error "unknow command " m))))
   dispatch)
 
-(define (evaluate-function function params-values)
+(define (apply function params-values)
   (let ((env (create-environment (function 'get-env)))
         (body (function 'get-body))
         (params-names (function 'get-params)))
+    
     (define (bind-params rest-params rest-values)
       (if (null? rest-params) 'done-binding
           (begin
              ((env 'set-binding!) (car rest-params) (ev [car rest-values] env))
              (bind-params (cdr rest-params) (cdr rest-values)))))
     (bind-params params-names params-values)
+
     (define (evaluate-body rest-body)
       (if (null? (cdr rest-body)) (ev (car rest-body) env)
           (begin (ev (car rest-body) env)
                  (evaluate-body (cdr rest-body)))))
     (evaluate-body body)))
 
-;;tests
-(define g (create-environment '()))
-(define env (create-environment g))
-([env 'set-binding!] 'a 1)
-([env 'set-binding!] 'b 2)
-;(ev '(+ a b) env)
+;; tests
 
-(define add (create-function '(a b) (list '(- (* a 10) b)) env))
-(evaluate-function add (list 1 2))
+(define (setup-test-envs)
+  ;;returns a tuples with (g . env)
+  ;;g is the global frame
+  ;;env is a child
+  ;; env sees two bindings 'a: 1 and 'b: 2
+   (define g (create-environment '()))
+   (define env (create-environment g))
+   ([g 'set-binding!] 'a 1)
+   ([env 'set-binding!] 'b 2)
+  (cons g env))
+  
+(test-begin
+ "Testing the environment API"
+ (define envs (setup-test-envs))
+ (let ([g (car envs)]
+       [env (cdr envs)])
+   (check-equal? ([env 'get-binding] 'a) 1 "retrieving value of 'a")
+   (check-equal? ([env 'get-binding] 'b) 2 "retrieving value of 'b")))
+
+(test-begin
+ "Testing the evaluation of primitive expressions"
+ (define envs (setup-test-envs))
+ (let ([g (car envs)]
+       [env (cdr envs)])
+   (check-equal? [ev '2 env] 2 "evaluating quoted expressions")
+   (check-equal? [ev '(+ 1 2) env] 3 "evaluating (+ 1 2)")
+   (check-equal? [ev '(* 1 2) env] 2 "evaluating (* 1 2)")
+   (check-equal? [ev '(- 1 2) env] -1 "evaluating (- 1 2)")
+   (check-equal? [ev '(/ 1 2) env] (/ 1 2) "evaluating (/ 1 2)")))
+
+(test-begin
+ "Testing the definition and evaluation of functions"
+ (define envs (setup-test-envs))
+ (let ([g (car envs)]
+       [env (cdr envs)])
+   ((env 'set-binding!) 'f1 (create-function '(a b) (list '(- (* a 2) b)) env))
+   (check-equal? [ev '(f1 2 6)  env] -2 "evaluating quoted expressions")
+   ))
+
